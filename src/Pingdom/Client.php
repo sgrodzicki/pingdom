f8957cf03c03d44441502aab326c7fddb9107652
Thomas Meeus <thomas.meeus@kunstmaan.be>
2014-08-29 10:12:46 +0200

    add updatehttp-check

--- src/Pingdom/Client.php
+++ src/Pingdom/Client.php
@@ -153,6 +153,30 @@ class Client
 
 		return $response;
 	}
+	public function updateHTTPCheck($name, $host, $url, $sendtoemail, $sendtoiphone, $sendtoandroid, $contactids) {
+		$client = new \Guzzle\Service\Client('https://api.pingdom.com/api/2.0');
+
+		/** @var $request \Guzzle\Http\Message\Request */
+		$request = $client->post('checks/' . $checkId , array('App-Key' => $this->token));
+		$request->setAuth($this->username, $this->password);
+
+		$query = $request->getQuery();
+
+		$query->set('name', $name);
+		$query->set('host', $host);
+		$query->set('type', 'http');
+		$query->set('url', $url);
+		$query->set('sendtoemail', $sendtoemail);
+		$query->set('sendtoiphone', $sendtoiphone);
+		$query->set('sendtoandroid', $sendtoandroid);
+		$query->set('contactids', $contactids);
+		$query->set('use_legacy_notifications', 'true');
+
+		$response = $request->send();
+		$response = json_decode($response->getBody(), true);
+
+		return $response;
+	}
 	public function removeCheck($checkId) {
 		$client = new \Guzzle\Service\Client('https://api.pingdom.com/api/2.0');
 
